01e7a6de563b5055b95d5d92b7c471fece6e2024
jalayrupera <jalayrupera13@gmail.com>
2022-01-17 12:04:13 +0530

    Typo fixed

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function StreamList() {
     return (
         <div>
-            StreamEdit Component
+            StreamList Component
         </div>
     )
 }
