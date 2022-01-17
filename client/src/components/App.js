54301f93160ca5c5e24fe151523ac397bb725644
jalayrupera <jalayrupera13@gmail.com>
2022-01-17 12:10:38 +0530

    Routes added for components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { BrowserRouter, Link, Route} from 'react-router-dom';
+import StreamList from './streams/StreamList'
+import StreamEdit from './streams/StreamEdit'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamShow from './streams/StreamShow'
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                App Component   
-            </div>
-        )
-    }
+
+
+export default function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                   <Route path="/" exact component={StreamList}/>
+                   <Route path="/streams/new" component={StreamCreate}/>
+                   <Route path="/streams/edit" component={StreamEdit}/>
+                   <Route path="/streams/delete" component={StreamDelete}/>
+                   <Route path="/streams/show" component={StreamShow}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
